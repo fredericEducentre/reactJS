23849054c0d7e61a6e780a4348bac7c94f0d0696
Frédéric Bui <bui.frederic.pro@gmail.com>
2024-05-12 18:08:36 +0200

    square meter calcul function

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+const { divide, surface } = require('./components/surface.js');
+
+test('test surface', () => {
+    expect(surface(2)).toBe(31.80);
+    expect(surface(1)).toBe(15.90);
+});
