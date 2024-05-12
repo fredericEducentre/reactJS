fa4ec4d30f71358bed45c6a65242eb14adb64b4c
Frédéric Bui <bui.frederic.pro@gmail.com>
2024-05-12 22:43:16 +0200

    try fix build

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">EstimationChauffageM2</a>
+        <a className="navbar-brand" href="/">EstimationChauffageM2</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -20,10 +20,10 @@ function App() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Calcul</a>
+              <a className="nav-link" href="/">Calcul</a>
             </li>
           </ul>
         </div>
